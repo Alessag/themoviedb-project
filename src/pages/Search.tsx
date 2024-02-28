100902cd1c6e456fe92b33e0ed612471dc3ae8d6
Alessandra Amicarella <17497470+Alessag@users.noreply.github.com>
2024-02-28 01:36:57 +0100

    Add pagination

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,16 +1,18 @@
 import { useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
+import { keepPreviousData, useQuery } from '@tanstack/react-query';
 import { Col, Row } from 'antd';
 
 import { MovieService } from '../utils/services/movie.service';
 
 const Search = () => {
   const [search, setSearch] = useState<string>('');
+  const [page, setPage] = useState<number>(1);
   const movieService = new MovieService();
 
   const query = useQuery({
-    queryKey: ['movies', search],
-    queryFn: () => movieService.searchMovies({ query: search }),
+    queryKey: ['movies', page, search],
+    queryFn: () => movieService.searchMovies({ query: search, page: page }),
+    placeholderData: keepPreviousData,
   });
 
   return (
@@ -32,6 +34,29 @@ const Search = () => {
           ))}
         </ul>
       </Row>
+      <span>Current Page: {page}</span>
+      <button
+        onClick={() => setPage((old) => Math.max(old - 1, 1))}
+        disabled={page === 1}
+      >
+        Previous Page
+      </button>{' '}
+      <button
+        onClick={() => {
+          if (
+            !query.isPlaceholderData &&
+            (query.data?.page ?? 0 < (query.data?.total_pages ?? 0))
+          ) {
+            setPage((old) => old + 1);
+          }
+        }}
+        disabled={
+          query.isPlaceholderData ||
+          !(query.data?.page ?? 0 < (query.data?.total_pages ?? 0))
+        }
+      >
+        Next Page
+      </button>
     </div>
   );
 };
