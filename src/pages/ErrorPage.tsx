24c73ddbb46fa5d547076a8912387acc2c7d26da
Alessandra Amicarella <17497470+Alessag@users.noreply.github.com>
2024-03-01 15:02:39 +0100

    Error page responsive styles

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,23 +1,28 @@
 import { Link, useRouteError } from 'react-router-dom';
-import { Col, Row } from 'antd';
+import { Button, Col, Row } from 'antd';
 
 const ErrorPage = () => {
   const error = useRouteError() as { statusText: string; data: string };
 
   return (
-    <Row
-      id="error-page"
-      justify="center"
-    >
-      <Col>
-        <h1>Oops!</h1>
-        <p>Sorry, an unexpected error has occurred.</p>
-        <p>
-          <i>{error.statusText || error.data}</i>
-        </p>
-        <Link to="/">Home</Link>
-      </Col>
-    </Row>
+    <div className="h-screen mx-auto flex justify-center">
+      <Row id="error-page">
+        <Col className="text-center">
+          <h1 className="text-2xl sm:text-5xl md:text-7xl font-bold md:mb-4">
+            Oops!
+          </h1>
+          <h2 className="text-gray-700 sm:text-2xl md:text-3xl  my-2 md:my-4">
+            Sorry, an unexpected error has occurred
+          </h2>
+          <p className="text-sm sm:text-xl text-gray-600  mb-2 md:my-3">
+            <i>{error.statusText || error.data}</i>
+          </p>
+          <Link to="/">
+            <Button type="link">Home page</Button>
+          </Link>
+        </Col>
+      </Row>
+    </div>
   );
 };
 
