d46e9b699b68df9a5dcd4c053e67c76d9a1d08d9
Alessandra Amicarella <17497470+Alessag@users.noreply.github.com>
2024-03-02 15:26:37 +0100

    Refactor service movie unit tests

--- tests/services/movieService.test.ts
+++ tests/services/movieService.test.ts
@@ -3,41 +3,41 @@ import fetchMock from 'jest-fetch-mock';
 import { MovieService } from '../../src/utils/services/movie.service';
 import { mockMovieApiResponse } from '../utils/mock-data';
 
-fetchMock.enableMocks();
+describe('MovieService', () => {
+  let movieService: MovieService;
 
-describe('movieService', () => {
   beforeEach(() => {
-    fetchMock.resetMocks(); // Reset fetch mocks before each test
+    movieService = new MovieService();
+    fetchMock.enableMocks();
   });
 
-  it('fetches popular movies', async () => {
-    // Mock the fetch call to return a successful response
-    fetchMock.mockResponseOnce(JSON.stringify(mockMovieApiResponse));
-
-    const service = new MovieService();
-
-    const data = await service.getPopularMovies();
-
-    // Assertions to verify the service behaves as expected
-    expect(data.results).toBeInstanceOf(Array);
-    expect(data.results).not.toHaveLength(0);
-    expect(data.total_results).toBeGreaterThan(0);
-    expect(data.total_pages).toBeGreaterThan(0);
-    expect(data.page).toBe(1);
-    expect(fetchMock).toHaveBeenCalledTimes(1);
+  afterEach(() => {
+    fetchMock.resetMocks();
   });
 
-  it('fetches it fails without the valid key fails ', async () => {
-    // Mock the fetch call to simulate a failure
-    fetchMock.mockReject(
-      new Error('Invalid API key: You must be granted a valid key.'),
-    );
+  describe('getPopularMovies', () => {
+    it('Should return the popular movies', async () => {
+      fetchMock.mockResponseOnce(JSON.stringify(mockMovieApiResponse));
 
-    const service = new MovieService();
+      const popularMovies = await movieService.getPopularMovies();
 
-    // Assert that the service throws an error as expected
-    await expect(service.getPopularMovies()).rejects.toThrow(
-      'Invalid API key: You must be granted a valid key.',
-    );
+      expect(popularMovies.results).toBeInstanceOf(Array);
+      expect(popularMovies.results).not.toHaveLength(0);
+      expect(popularMovies.total_results).toBeGreaterThan(0);
+      expect(popularMovies.total_pages).toBeGreaterThan(0);
+      expect(popularMovies.page).toBe(1);
+      expect(fetchMock).toHaveBeenCalledTimes(1);
+    });
+
+    it('Should throw and error when the API request fails without the valid key', async () => {
+      fetchMock.mockReject(
+        new Error('Invalid API key: You must be granted a valid key.'),
+      );
+
+      const service = new MovieService();
+      await expect(service.getPopularMovies()).rejects.toThrow(
+        'Invalid API key: You must be granted a valid key.',
+      );
+    });
   });
 });
